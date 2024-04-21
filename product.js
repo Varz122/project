160f67be503546e9fa74df7ecf5d9c3816b6e595
Varz122 <154825567+Varz122@users.noreply.github.com>
2024-04-21 14:01:47 +0530

    Add files via upload

--- product.js
+++ product.js
@@ -1,12 +1,12 @@
-const path = require('path')
-
-exports.addprod = (req,res,next)=>{
-  
-    res.sendFile(path.join(__dirname, '../', 'views' ,'add-product.html'))
-    
-}
-exports.postaddprod = (req,res,next)=>{
-    console.log(req.body);
-    res.redirect('/');
- 
+const products = [];
+module.exports = class Product {
+    constructor(t){
+        this.title = t;
+    }
+    save(){
+      products.push(this)
+    }
+    static fetchAll(){
+      return products
+    }
 }
